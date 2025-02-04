ef5dae919f5148f39812136620cb7d2c4b2b7186
elldia <elldia1222@gmail.com>
2025-02-04 09:12:39 +0900

    250223

---
+++ javascript/plugin/script/swiper_op5.js
@@ -0,0 +1,9 @@
+//const newSlide = new Swiper('.new_slide_wrap'); //기본형태
+const newSlide = new Swiper('.new_slide_wrap',{
+    mousewheel:true, //웹사이트 수직페이지 전환시에만 사용
+    direction:'vertical',
+});
+
+const snsSlide = new Swiper('.sns_portfolio',{
+    
+});
